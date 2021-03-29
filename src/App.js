c5e8d0c308762a0fb323143f3af0f0ed00d0159a
jgraykeyin <justin.gray@keyin.com>
2021-03-29 15:31:59 -0230

    Moved json to public folder, added ReactPlayer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
+import { React, useEffect, useState } from 'react';
+import ReactPlayer from "react-player"
 import './App.css';
 
 function App() {
+
+  const [artistName, setArtistName] = useState("Alice Cooper");
+  const [songAddress, setSongAddress] = useState("");
+
+  // Do this as soon as the app loads
+  useEffect(()=>{
+    console.log("Bleep bloop say this once");
+    
+    fetch('data/songs.json')
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json[0]["artist"]);
+      
+      setArtistName(json[0]["artist"]);
+      setSongAddress(json[0]["url"]);
+
+    });
+  });
+
   return (
     <div className="App">
       <h1>Karoaoke party!</h1>
+      <p>Artist name: {artistName}</p>
+
+      <div className="PlayerArea">
+        <ReactPlayer width="100vw" url={songAddress} />
+      </div>
     </div>
   );
 }
