41a85b137cd49a7ecb08725b8e9c75a90d2421f5
Ashley Mercer <ashleymercer@admins-MacBook-Pro.local>
2021-03-30 16:00:12 -0230

    button css update

--- src/home_page.js
+++ src/home_page.js
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom'
 function HomePage() {
   return (
     <div className="App">
-        <h1>Okie Dokie Karaoke</h1>
+        <h1 className="Header">Okie Dokie Karaoke</h1>
         <Link to="/song">
-            <button>Click</button>
+            <button className="Sing-btn">Click</button>
         </Link>
     </div>
   );
