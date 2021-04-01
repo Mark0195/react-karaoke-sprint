cd227349faf26ac1d19bd425147c8dcd5216a234
jgraykeyin <justin.gray@keyin.com>
2021-04-01 13:46:11 -0230

    Added a Theme CSS file

--- src/home_page.js
+++ src/home_page.js
@@ -1,17 +1,24 @@
 import { React } from 'react';
 import './App.css';
+import './ThemeMainStream.css';
 import { Link } from 'react-router-dom'
 
 function HomePage() {
   return (
     <div className="App">
+
+      <div className="Button-area">
         <h1 className="Header">Okie Dokie Karaoke</h1>
+
         <Link to="/song">
-            <button className="Sing-btn">Click</button>
+            <button className="Sing-btn">Start Singing</button>
         </Link>
+
         <Link to="/browse">
-            <button className="browse-btn">Browse</button>
+            <button className="Sing-btn">Browse Playlist</button>
         </Link>
+      </div>
+
     </div>
   );
 }
