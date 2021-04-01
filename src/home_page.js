f8ceab009f96cca78b87ddf2b88a6c42600478be
Ashley Mercer <ashleymercer@admins-MacBook-Pro.local>
2021-04-01 14:35:50 -0230

    Main-stream css changes

--- src/home_page.js
+++ src/home_page.js
@@ -5,10 +5,12 @@ import { Link } from 'react-router-dom'
 
 function HomePage() {
   return (
-    <div className="App">
+    <div className="Main-container">
 
       <div className="Button-area">
         <h1 className="Header">Okie Dokie Karaoke</h1>
+
+        <img className="Music-note" src="./images/music_note.png"></img>
 
         <Link to="/song">
             <button className="Sing-btn">Start Singing</button>
