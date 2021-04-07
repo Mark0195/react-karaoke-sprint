20b5863245315c139b5028595dd8d98967174fbd
Mark0195 <mark.myles@keyin.com>
2021-04-07 11:16:57 -0230

    Music Library css

--- src/browse_music.js
+++ src/browse_music.js
@@ -1,6 +1,6 @@
 import { React, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
+import "./ThemeMainStream.css"
 
 function BrowseMusic() {
 
@@ -17,7 +17,7 @@ function BrowseMusic() {
             // Loop through each song and add the html into an array
            let songHtml = []
            for (let i = 0; i < response.length; i++) {
-               let html = <Link to={"/song/"+i}><div className="BrowseItem" key={i}><h2>{response[i]["artist"]}</h2><p>{response[i]["song"]}</p> <img className="SongImage" src={"images/"+response[i]["image"]}></img></div></Link>
+               let html = <Link to={"/song/"+i}><div className="BrowseItem" key={i}><h2 className="BrowseArtistName">{response[i]["artist"]}</h2><p className="BrowseSongName">{response[i]["song"]}</p><img className="SongImage" src={"images/"+response[i]["image"]}></img></div></Link>
                songHtml.push(html)
            }
            setSongList(songHtml)
@@ -27,8 +27,8 @@ function BrowseMusic() {
     },[]);
 
     return (
-        <div className="App">
-            <h1 className="Header">Okie Dokie Karaoke</h1>
+        <div className="MainContainer">
+            <h1 className="BrowseHeader">Music Library</h1>
             <div className="BrowseContainer">{songList}</div>
         </div>
     );
